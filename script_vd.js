8394c5aacd5be699e2db0b78d85095833aa79928
nguyenmen0312 <nguyenthimen03122001@gmail.com>
2020-12-02 15:25:17 +0700

    btvn

--- script_vd.js
+++ script_vd.js
@@ -11,10 +11,39 @@ hello = () => {
     console.log("mang sau khi xóa phần tử chỉ số thứ 5: " + arr);
     //dùng từ khóa MAP tìm ra 1 mảng mới nhân 3 giá trị mảng lên 
     let arr1 = [1, 2, 3, 4];
-    let arrnew = arr1.map(i => i * 3);
+    let arrnew = arr1.map(x => x * 3);
     console.log(arrnew);
 }
 hello()
+//if else if else
+istestelseif = () => {
+    let myname = 'hehe';
+    if (myname == 'menngu') {
+        alert('OKe menngu');
+    }
+    else if (myname == 'menkhongngu') {
+        alert('false menngu');
+    }
+    else { alert('men bth') };
+}
+istestelseif();
+//setTimeout
+// function hello() {
+//     alert("xin chao mến");
+// }
+// function hienthi(){
+//     setTimeout (function()){
+//     alert (hello());
+// },2000)}
+// hienthi()
+//vòng lặp for
+function testfor() {
+    let arr = [1,2,3,4,5,6,7,8,9,10];
+    for (let i = 0; i< arr.length; i++) {
+        alert(arr[i]);
+    }
+}
+testfor()
 
 
 
